15e285f436e2b029e3d032977467659b467505d1
Tushar Totla <tushartotla12@gmail.com>
2026-09-20 15:18:57 +0530

    custom exceptions

---
+++ src/main/java/org/dmg/Exception/BusinessRuleViolationException.java
@@ -0,0 +1,9 @@
+package org.dmg.Exception;
+
+public class BusinessRuleViolationException extends RuntimeException {
+
+    public BusinessRuleViolationException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/org/dmg/Exception/ConflictException.java
@@ -0,0 +1,9 @@
+package org.dmg.Exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
+
