eceab26676b85fab5d7b9e857b84a4438339ef56
Tushar Totla <tushartotla12@gmail.com>
2026-09-20 13:23:06 +0530

    initial application run test

---
+++ src/main/java/org/dmg/FoodDeliveryManagment.java
@@ -0,0 +1,12 @@
+package org.dmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FoodDeliveryManagment {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryManagment.class, args);
+    }
+}
